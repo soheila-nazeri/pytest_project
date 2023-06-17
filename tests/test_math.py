9c2ba5f256694ecf98bce0e5757fc3d826006dc5
soheila-nazeri <s.nazeri2009@gmail.com>
2023-06-17 18:00:26 +0300

    init
    init

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,41 +1,54 @@
 # test_math_operations.py
 
 import pytest
-from  pytestproject.math import add, subtract, multiply, divide
+from pytestproject.math import add, subtract, multiply, divide
+
 
 class MathOperationsTest:
-    @pytest.mark.parametrize("a, b, expected", [
-        (5, 2, 7),     # a + b = 5 + 2 = 7
-        (-10, 3, -7),  # a + b = -10 + 3 = -7
-        (0, 0, 0)      # a + b = 0 + 0 = 0
-    ])
-    def test_add(self, a, b, expected):
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (5, 2, 7),  # a + b = 5 + 2 = 7
+            (-10, 3, -7),  # a + b = -10 + 3 = -7
+            (0, 0, 0),  # a + b = 0 + 0 = 0
+        ],
+    )
+    def test_add(lself, a, b, expected):
         result = add(a, b)
         assert result == expected
 
-    @pytest.mark.parametrize("a, b, expected", [
-        (10, 3, 7),    # a - b = 10 - 3 = 7
-        (5, -2, 7),    # a - b = 5 - (-2) = 7
-        (0, 0, 0)      # a - b = 0 - 0 = 0
-    ])
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (10, 3, 7),  # a - b = 10 - 3 = 7
+            (5, -2, 7),  # a - b = 5 - (-2) = 7
+            (0, 0, 0),  # a - b = 0 - 0 = 0
+        ],
+    )
     def test_subtract(self, a, b, expected):
         result = subtract(a, b)
         assert result == expected
 
-    @pytest.mark.parametrize("a, b, expected", [
-        (5, 2, 10),     # a * b = 5 * 2 = 10
-        (-3, 4, -12),   # a * b = -3 * 4 = -12
-        (0, 10, 0)      # a * b = 0 * 10 = 0
-    ])
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (5, 2, 10),  # a * b = 5 * 2 = 10
+            (-3, 4, -12),  # a * b = -3 * 4 = -12
+            (0, 10, 0),  # a * b = 0 * 10 = 0
+        ],
+    )
     def test_multiply(self, a, b, expected):
         result = multiply(a, b)
         assert result == expected
 
-    @pytest.mark.parametrize("a, b, expected", [
-        (10, 2, 5),     # a / b = 10 / 2 = 5
-        (5, -2, -2.5),  # a / b = 5 / (-2) = -2.5
-        (8, 4, 2),      # a / b = 8 / 4 = 2
-    ])
+    @pytest.mark.parametrize(
+        "a, b, expected",
+        [
+            (10, 2, 5),  # a / b = 10 / 2 = 5
+            (5, -2, -2.5),  # a / b = 5 / (-2) = -2.5
+            (8, 4, 2),  # a / b = 8 / 4 = 2
+        ],
+    )
     def test_divide(self, a, b, expected):
         result = divide(a, b)
         assert result == expected
